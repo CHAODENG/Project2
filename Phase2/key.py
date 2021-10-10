ee9c605d0f65c1a0fb59ca45a03f1c3e64fac911
CHAODENG <90479627+CHAODENG@users.noreply.github.com>
2021-10-09 22:10:18 -0400

    Add files via upload

---
+++ Phase2/key.py
@@ -0,0 +1,12 @@
+import json
+
+# First, entering your keys/secrets as strings in the following fields
+credentials = {}
+credentials['CONSUMER_KEY'] = 'Enter your consumer key'
+credentials['CONSUMER_SECRET'] = 'Enter your consumer secret'
+credentials['ACCESS_TOKEN'] = 'Enter your access token'
+credentials['ACCESS_SECRET'] = 'Enter your access secret'
+
+# Saving the credentials object to json file
+with open("twitter_credentials.json", "w") as file:
+    json.dump(credentials, file)
