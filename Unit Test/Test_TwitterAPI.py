f75082d1b4675bb4faf7c2f2a3fd47b0782de36d
CHAODENG <90479627+CHAODENG@users.noreply.github.com>
2021-10-30 16:24:53 -0400

    Update Test_TwitterAPI.py

--- Unit Test/Test_TwitterAPI.py
+++ Unit Test/Test_TwitterAPI.py
@@ -5,10 +5,10 @@ import sys
 
 tweets = TAPI.get_authorization()
 class Unit_Test(unittest.TestCase):
-    #def test_credentials(self):
-    #    with open('my_credentials_info.json','r') as file:
-    #        credentials = json.load(file)
-    #    self.assertEqual(TAPI.get_my_credentials(tweets),credentials,"Error")
+    def test_credentials(self):
+        with open('my_credentials_info.json','r') as file:
+            credentials = json.load(file)
+        self.assertEqual(TAPI.get_my_credentials(tweets),credentials,"Error")
     def test_home_timeline(self):
         with open('my_home_timeline.json','r') as file:
             my_home_timeline = json.load(file)
