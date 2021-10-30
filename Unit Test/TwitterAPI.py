12649616e08205a81ffa106ba605ec65f3c77575
CHAODENG <90479627+CHAODENG@users.noreply.github.com>
2021-10-30 14:47:29 -0400

    Update TwitterAPI.py

--- Unit Test/TwitterAPI.py
+++ Unit Test/TwitterAPI.py
@@ -1,2 +1,38 @@
+from twython import Twython
+import pandas as pd
+import json
+
+# for security, we can load credentials from json file
+# before this step, you should use key.py to generate your json file
+with open("twitter_credentials.json", "r") as file:
+    creds = json.load(file)
+
 # Instantiate an object
-tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'], creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
+tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'],creds['ACCESS_TOKEN'],creds['ACCESS_SECRET'])
+credential = tweets.verify_credentials()
+print(tweets.get_home_timeline())
+
+print(credential)
+# Create a query:    q, result_type, count and lang
+# respectively for the search keyword, type, count, and language of results.
+query = {
+        'q': 'Boston celtics',  # search keyword
+        'result_type': 'popular',  # search type
+        'count': 30,   # count
+        'lang': 'en', # languages of results
+        }
+
+# Search tweets for query's information
+dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}
+for status in tweets.search(**query)['statuses']:
+    dict_['user'].append(status['user']['screen_name'])
+    dict_['date'].append(status['created_at'])
+    dict_['text'].append(status['text'])
+    dict_['favorite_count'].append(status['favorite_count'])
+
+with open("twitter_info.json", "w") as file:
+    json.dump(dict_, file,indent=4)
+with open("credentials.json", "w") as file:
+    json.dump(credential, file,indent=4)
+
+print(dict_)
