c2e9055bc8cae1b1bffd0b435890169e49f3fc99
CHAODENG <90479627+CHAODENG@users.noreply.github.com>
2021-10-30 15:55:59 -0400

    Update Test_TwitterAPI.py

--- Unit Test/Test_TwitterAPI.py
+++ Unit Test/Test_TwitterAPI.py
@@ -3,7 +3,7 @@ import TwitterAPI as TAPI
 import json
 import sys
 
-api = TAPI.get_authorization()
+tweets = TAPI.get_authorization()
 class Unit_Test(unittest.TestCase):
     #def test_credentials(self):
     #    with open('my_credentials_info.json','r') as file:
